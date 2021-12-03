bc8a586cb29bfe898486f0b42449b65760252db9
GuyTurevich <guy938@gmail.com>
2021-12-03 16:47:54 +0200

    first

---
+++ test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,24 @@
+package bgu.spl.mics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FutureTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void resolve() {
+    }
+
+    @Test
+    void isDone() {
+    }
+
+    @Test
+    void testGet() {
+    }
+}
