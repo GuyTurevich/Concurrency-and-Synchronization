39e79a2dae8b3c4a060b4c3d13c6def84b65d32d
Nitay Maoz <maoznit@post.bgu.ac.il>
2021-12-15 12:36:37 +0200

    MessageBusImpl almost done - need to complete unregister

--- main/java/bgu/spl/mics/MessageBusImpl.java
+++ main/java/bgu/spl/mics/MessageBusImpl.java
@@ -3,6 +3,7 @@ package bgu.spl.mics;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -14,14 +15,14 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class MessageBusImpl implements MessageBus {
 	private static MessageBusImpl singleton;
 	private ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Message>> messageQueues;
-	private ConcurrentHashMap<Event<?>, LinkedList<MicroService>> eventsSubscribers;
-	private ConcurrentHashMap<Broadcast , LinkedList<MicroService>> broadcasts;
+	private ConcurrentHashMap<Class<? extends Event>, LinkedList<MicroService>> eventsSubscribers;
+	private ConcurrentHashMap<Class<? extends Broadcast> , LinkedList<MicroService>> broadcasts;
 	private ConcurrentHashMap<Event, Future> eventFuture; //stores current events + their future obj
 
 	public MessageBusImpl(){
 		this.messageQueues = new ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Message>>();
-		this.eventsSubscribers = new ConcurrentHashMap<Event<?>,LinkedList<MicroService>>();
-		this.broadcasts = new ConcurrentHashMap<Broadcast , LinkedList<MicroService>>();
+		this.eventsSubscribers = new ConcurrentHashMap<Class<? extends Event>,LinkedList<MicroService>>();
+		this.broadcasts = new ConcurrentHashMap<Class<? extends Broadcast> , LinkedList<MicroService>>();
 		this.eventFuture = new ConcurrentHashMap<Event,Future>();
 	}
 
@@ -37,8 +38,12 @@ public class MessageBusImpl implements MessageBus {
 	 * @PRE message_queues !=null , events !=null
 	 */
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+		synchronized (eventsSubscribers) {
+			eventsSubscribers.putIfAbsent(type, new LinkedList<MicroService>());
+			eventsSubscribers.get(type).add(m);
+		}
 
+		//Maybe need to add a reverse hashmap if we need to get events list by key - Microservice
 	}
 
 	/**
@@ -49,7 +54,12 @@ public class MessageBusImpl implements MessageBus {
 	 */
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+		synchronized (broadcasts){
+			broadcasts.putIfAbsent(type,new LinkedList<MicroService>());
+			broadcasts.get(type).add(m);
+
+			//Maybe need to add a reverse hashmap if we need to get broadcasts list by key - Microservice
+		}
 
 	}
 
@@ -69,7 +79,6 @@ public class MessageBusImpl implements MessageBus {
 			eventFuture.get(e).notifyAll();
 		}
 		eventFuture.remove(e);
-
 	}
 
 
@@ -82,8 +91,17 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
-
+		LinkedList<MicroService> subs = broadcasts.get(b);
+		synchronized (broadcasts) {// in case of unregister we will have out of bounds Exception.
+			for (int i = 0; i < subs.size(); i++) {
+				ConcurrentLinkedQueue<Message> messages =messageQueues.get(subs.get(i)); //get Microservice message queue
+				messages.add(b);
+				if (messages.size()==1) //if the message we added is the only message in queue we will notify the queue
+					synchronized (messages){
+					messages.notify();
+					}
+			}
+		}
 	}
 
 	/**
@@ -97,8 +115,26 @@ public class MessageBusImpl implements MessageBus {
 	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		Future future =new Future<T>();
+		eventFuture.put(e,future);
+		LinkedList<MicroService>  subs= eventsSubscribers.get(e.getClass());
+		if (subs==null || subs.size()==0){
+			return null;
+		}
+		MicroService microService ;
+		synchronized (subs){
+			microService = subs.peek();
+		}
+
+		ConcurrentLinkedQueue<Message> messages = messageQueues.get(microService);
+		messageQueues.get(microService).add(e); // adding the event to messagesQueues
+
+		if (messages.size()==1){ //if the message we added is the only message in queue we will notify the queue
+			synchronized (messages){
+				messages.notify();
+			}
+		}
+		return future;
 	}
 
 
@@ -107,7 +143,6 @@ public class MessageBusImpl implements MessageBus {
 
 		if(!messageQueues.containsKey(m))
 			messageQueues.put(m,new ConcurrentLinkedQueue<Message>());
-
 	}
 
 	/**
@@ -132,7 +167,6 @@ public class MessageBusImpl implements MessageBus {
 	 */
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-
 		ConcurrentLinkedQueue<Message> queue = messageQueues.get(m);
 		if (queue.isEmpty()) {
 			synchronized (queue) {
